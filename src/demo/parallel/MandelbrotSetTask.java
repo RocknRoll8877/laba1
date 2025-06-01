2a2be24f8f17a70f84f46b8ff32b9a36e5a4188f
Дубовский М.А. 150541 <matvey.dubovskiy.100@gmail.com>
2025-06-02 00:05:51 +0300

    изменена цветовая гамма

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -351,20 +351,20 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
-            Color.WHITE,
-            Color.RED,
-            Color.rgb(100, 0, 0),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+                Color.DARKBLUE,
+                Color.BLUE,
+                Color.PURPLE,
+                Color.MEDIUMPURPLE,
+                Color.LAVENDER,
+                Color.LIGHTGRAY,
+                Color.WHITE
         };
         
         /**
          * Color stops for colors table: relative position in the table
          */
         double[] cp = {
-            0, 0.17, 0.25, 0.30, 0.5, 0.75, 1,};
+            0, 0.20, 0.35, 0.50, 0.68, 0.84, 1,};
         
         /**
          * Color table population
